syberia-project/platform_packages_apps_Settings

SmartCharge: update for HAL implementation
Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: I5d0fc65aee115de7d179058dc0563482288fd75a

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -32,6 +32,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.provider.Settings.Global;
 import android.text.format.Formatter;
@@ -65,7 +66,10 @@ import com.android.settingslib.utils.PowerUtil;
 import com.android.settingslib.utils.StringUtil;
 import com.android.settingslib.widget.LayoutPreference;
 
+import vendor.syberia.smartcharge.V1_0.ISmartCharge;
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Displays a list of apps and subsystems that consume power, ordered by how much power was
@@ -86,6 +90,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_SCREEN_USAGE = "screen_usage";
     private static final String KEY_BATTERY_TEMP = "battery_temp";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
+
+    private ISmartCharge mSmartCharge = null;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -257,9 +263,21 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
         // Check availability of Smart Charging
         Preference mSmartCharging = (Preference) findPreference("smart_charging_key");
-        if (!getResources().getBoolean(R.bool.config_supportSmartCharging)) {
+        if (getSmartCharge() ==  null) {
             getPreferenceScreen().removePreference(mSmartCharging);
         }
+    }
+
+    private synchronized ISmartCharge getSmartCharge() {
+        try {
+            return ISmartCharge.getService();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (NoSuchElementException ex) {
+            // service not available
+        }
+
+        return null;
     }
 
     @Override

CAF-Extended/packages-apps-Settings

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -26,6 +26,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.RemoteException;
 import android.provider.Settings.Global;
 import android.text.format.Formatter;
 import android.view.Menu;
@@ -55,7 +56,10 @@ import com.android.settingslib.utils.PowerUtil;
 import com.android.settingslib.utils.StringUtil;
 import com.android.settingslib.widget.LayoutPreference;
 
+import vendor.aosp.smartcharge.V1_0.ISmartCharge;
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Displays a list of apps and subsystems that consume power, ordered by how much power was consumed
@@ -72,6 +76,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_SCREEN_USAGE = "screen_usage";
     private static final String KEY_BATTERY_TEMP = "battery_temp";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
+
+    private ISmartCharge mSmartCharge = null;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -236,9 +242,21 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
         // Check availability of Smart Charging
         Preference mSmartCharging = (Preference) findPreference("smart_charging_key");
-        if (!getResources().getBoolean(R.bool.config_supportSmartCharging)) {
+        if (getSmartCharge() ==  null) {
             getPreferenceScreen().removePreference(mSmartCharging);
         }
+    }
+
+    private synchronized ISmartCharge getSmartCharge() {
+        try {
+            return ISmartCharge.getService();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (NoSuchElementException ex) {
+            // service not available
+        }
+
+        return null;
     }
 
     @Override
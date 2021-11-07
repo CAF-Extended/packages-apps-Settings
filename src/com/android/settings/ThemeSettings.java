ff074ac9ba09c7cc2b49eb29f9ec4fe33dbddefd
shivatejapeddi <shivatejapeddi@gmail.com>
2021-11-08 02:02:06 +0530

    Settings : Add support for icon shapes, packs and fonts * Need to fix reset after reboot

--- src/com/android/settings/ThemeSettings.java
+++ src/com/android/settings/ThemeSettings.java
@@ -18,92 +18,69 @@ package com.android.settings;
 
 import static android.os.UserHandle.USER_SYSTEM;
 
-import android.app.ActivityManagerNative;
-import android.app.UiModeManager;
-import android.content.Context;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
-import android.content.om.IOverlayManager;
-import android.content.om.OverlayInfo;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Resources;
-import android.graphics.Color;
+import android.os.Bundle;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import androidx.annotation.VisibleForTesting;
-import androidx.preference.Preference;
-import androidx.preference.ListPreference;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.WindowManagerGlobal;
-import android.view.IWindowManager;
-import android.widget.Toast;
+import android.graphics.Color;
 
+import androidx.fragment.app.Fragment;
+import androidx.preference.*;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import com.android.settings.Utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.Utils;
-
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.OverlayCategoryPreferenceController;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.search.SearchIndexable;
 import com.cafex.support.colorpicker.ColorPickerPreference;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class ThemeSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+public class ThemeSettings extends DashboardFragment implements OnPreferenceChangeListener {
 
-    private static final String TAG = "ThemeSettings";
+    public static final String TAG = "ThemeSettings";
 
     private String MONET_ENGINE_COLOR_OVERRIDE = "monet_engine_color_override";
-
+    private ColorPickerPreference mMonetColor;  
     private Context mContext;
 
-    private ColorPickerPreference mMonetColor;
-
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.theme_settings);
-
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
         mContext = getActivity();
 
-        final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen screen = getPreferenceScreen();
-
-        mMonetColor = (ColorPickerPreference) screen.findPreference(MONET_ENGINE_COLOR_OVERRIDE);
+        mMonetColor = (ColorPickerPreference) prefScreen.findPreference(MONET_ENGINE_COLOR_OVERRIDE);
         int intColor = Settings.Secure.getInt(resolver, MONET_ENGINE_COLOR_OVERRIDE, Color.WHITE);
         String hexColor = String.format("#%08x", (0xffffff & intColor));
         mMonetColor.setNewPreviewColor(intColor);
         mMonetColor.setSummary(hexColor);
         mMonetColor.setOnPreferenceChangeListener(this);
+                
     }
 
-    @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.EXTENSIONS;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mMonetColor) {
@@ -117,4 +94,43 @@ public class ThemeSettings extends SettingsPreferenceFragment implements OnPrefe
         }
         return false;
     }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EXTENSIONS;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.theme_settings;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack.android"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));                
+        return controllers;
+    }
+
+    /**
+     * For Search.
+     */
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.theme_settings);
 }

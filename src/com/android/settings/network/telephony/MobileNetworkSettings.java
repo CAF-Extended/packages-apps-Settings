c194ee426a1ae7bf8748d4a6369f25b65c7866e5
shivatejapeddi <shivatejapeddi@gmail.com>
2021-12-25 11:07:43 +0530

    Revert "[VoNR] VoNR settings"
    This reverts commit eaa71ac6de150899ea5bc98aac1b6f58af1ca5a2.

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -239,7 +239,6 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings {
                 .addListener(videoCallingPreferenceController);
         use(ContactDiscoveryPreferenceController.class).init(getParentFragmentManager(), mSubId,
                 getLifecycle());
-        use(NrAdvancedCallingPreferenceController.class).init(mSubId);
     }
 
     @Override
